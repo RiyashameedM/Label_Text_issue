256ec82b0a94958f0800d311c02b104779e749b1
RiyashameedM <97871624+RiyashameedM@users.noreply.github.com>
2024-01-08 11:41:47 +0530

    Add files via upload

---
+++ MAUISample/MAUISample/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MAUISample
+{
+	public partial class MainPage : ContentPage
+	{
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked_1(object sender, EventArgs e)
+		{
+			label.Text = "Label text changed";
+		}
+	}
+
+}
---
+++ MAUISample/MAUISample/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MAUISample
+{
+	internal class ViewModel : INotifyPropertyChanged
+	{
+		private string selectedItem;
+
+		public string SelectedItem
+		{
+			get { return selectedItem; }
+			set
+			{
+				selectedItem = value;
+				OnPropertyChanged(nameof(SelectedItem));
+			}
+		}
+		public ICommand ListViewSelectionChangedCommand { get; set; }
+
+		public ViewModel()
+		{
+			this.SelectedItem = "old value";
+			ListViewSelectionChangedCommand = new Command(OnSelectionChanged);
+		}
+
+		private void OnSelectionChanged(object obj)
+		{
+			this.SelectedItem = "new value";
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string name)
+		{
+			if (this.PropertyChanged != null)
+				this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+		}
+	}
+}
